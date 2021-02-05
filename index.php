7bf72cd6b2b79a1ac5e36a60ed4e86c60f031c12
aivaras.v <aivaras.valasinas@gmail.com>
2021-02-05 20:35:03 +0000

    Initial file setup

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WatchTube</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+
+    <div id="pageContainer">
+
+        <div id="mastHeadContainer"> <!-- Top Bar -->
+        ff
+        </div>
+
+        <div id="sideNavContainer"> <!-- Side Menu -->
+
+        </div>
+
+        <div id="mainSectionContainer"> <!-- Main Content-->
+
+            <div id="mainContentContainer">
+                Hello everyone
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+</body>
+</html>
