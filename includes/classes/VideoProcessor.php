2e6cd48cf23ebface0e54d3489ea52018c6dac84
v-aivaras <aivaras.valasinas@gmail.com>
2021-02-07 19:07:37 +0000

    Working video upload

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -3,6 +3,8 @@
 class VideoProcessor {
 
     private $con;
+    private $sizeLimit = 500000000; 
+    private $allowedTypes = array("mp4", "flv", "webm", "mkv", "vob", "ogv", "ogg", "avi", "wmv", "mov", "mpg", "mpeg");
 
 
     public function __construct($con) {
@@ -19,13 +21,56 @@ class VideoProcessor {
 
         $tempFilePath = str_replace(" ", "_", $tempFilePath);
 
-        echo $tempFilePath;
+        $isValidData = $this->processData($videoData, $tempFilePath);
 
+        if(!$isValidData) {
+            return false;
+        }
 
+        if(move_uploaded_file($videoData["tmp_name"], $tempFilePath)) {
+            
+            $finalFilePath = $targetDir . uniqid() . ".mp4";
 
+            if(!$this->insertVideoData($videoUploadData, $finalFilePath)) {
+                echo "Insert query failed";
+                return false;
+            }
 
+        }
 
+    }
 
+    private function processData($videoData, $filePath) {
+        $videoType = pathInfo($filePath, PATHINFO_EXTENSION);
+
+        if(!$this->isValidSize($videoData)) {
+            echo "File too large. Can't be more than $this->sizeLimit bytes";
+            return false;
+        } else if (!$this->isValidType($videoType)) {
+            echo "Invalid file type";
+            return false;
+        } else if($this->hasError($videoData)) {
+            echo "Error code: " . $videoData["error"];
+            return false;
+        }
+        return true;
+    }
+
+    private function isValidSize($data) {
+        return $data["size"] <= $this->sizeLimit;
+    }
+
+    private function isValidType($type) {
+        $lowercased = strtolower($type);
+        return in_array($lowercased, $this->allowedTypes);
+    }
+
+    private function hasError($data) {
+        return $data["error"] !=0;
+    }
+
+    private function insertVideoData($uploadData, $filePath) {
+        
     }
 }
 
