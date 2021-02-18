49e1ff835d6c6d57a17c974720a04eff18eb01e0
v-aivaras <aivaras.valasinas@gmail.com>
2021-02-18 21:26:19 +0000

    profile.php page base

---
+++ includes/classes/ProfileData.php
@@ -0,0 +1,27 @@
+<?php
+
+class ProfileData {
+
+    private $con,  $profileUserObj;
+
+    public function __construct($con, $profileUsername) {
+        $this->con = $con;
+        $this->profileUserObj = new User($con, $profileUsername);
+    }
+
+    public function getProfileUsername() {
+        return $this->profileUserObj->getUsername();
+    }
+
+    public function userExists() {
+        $profileUsername = $this->getProfileUsername();
+        $query = $this->con->prepare("SELECT * FROM users WHERE username = :username");
+        $query->bindParam(":username", $profileUsername);
+        $query->execute();
+
+        return $query->rowCount() != 0;
+
+    }
+
+}
+?>
---
+++ includes/classes/ProfileGenerator.php
@@ -0,0 +1,22 @@
+<?php
+require_once("ProfileData.php");
+
+class ProfileGenerator {
+
+    private $con, $userLoggedInObj, $profileData;
+
+    public function __construct($con, $userLoggedInObj, $profileUsername) {
+        $this->con = $con;
+        $this->userLoggedInObj = $userLoggedInObj;
+        $this->profileData = new ProfileData($con, $profileUsername);
+    }
+
+    public function create() {
+        $profileUsername = $this->profileData->getProfileUsername();
+       
+        if(!$this->profileData->userExists()) {
+            return "User does not exist";
+        }
+    }
+}
+?>
